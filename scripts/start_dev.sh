0f530f6afa0a79863e1014bee39329d6bcedbf21
Tim Whitney <tim.d.whitney@gmail.com>
2017-04-23 23:08:47 -0600

    Added Script

---
+++ scripts/start_dev.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+cd $HOME/koding_drupal &&
+docker-compose exec php exec drush @dev si lightning -y;
+# Import database
+# docker-compose exec php exec drush @dev sql-drop drupal;
+# docker-compose exec php exec drush @dev sql-create drupal;
+# docker-compose exec mariadb exec mysql -udrupal -pdrupal drupal < ./database.sql
